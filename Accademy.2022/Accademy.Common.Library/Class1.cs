c7bff860a23d73f3d6cac00114a938835bee8bd1
Matthias C <74117251+mattiasUniPg@users.noreply.github.com>
2022-04-20 18:05:36 +0200

    common

---
+++ Accademy.2022/Accademy.Common.Library/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accademy.Common.Library
+{
+    public class CommonConstants
+    {
+        public const string AcademyName= "Microsoft .NET - 19 Aprile/31 Maggio";
+        public const int StartDay = 19;
+    }
+}
