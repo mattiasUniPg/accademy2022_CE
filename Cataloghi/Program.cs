5373e96248d6831488f7b18b41088b3ad1c502f4
Matthias C <74117251+mattiasUniPg@users.noreply.github.com>
2022-04-29 16:32:18 +0200

    Add files via upload

---
+++ Cataloghi/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Helper;
+
+var Prod = new Prodotto();
+var rub = Prod.Display();//TODO
+
+Console.WriteLine("*********************");
+
+foreach (var prodotto in rub)
+{
+    Console.WriteLine($"ID: {prodotto.IdProdotto} - Nome: {prodotto.NomeProdotto} - Giacenza: {prodotto.QntInMagazzino} ");
+}
+
+
+Console.WriteLine("*********************");
+
+Console.WriteLine("Inserisci ID");
+var IDPROD = Console.ReadLine();
+Console.WriteLine("Inserisci Nome_Prodotto");
+var NOMEPROD = Console.ReadLine();
+Console.WriteLine("Giacenza_in_Magazzino");
+var QNTMAGAZZINO = Console.ReadLine();
+
+
+var prodottonew = new Prodotto();
+prodottonew.IdProdotto = IDPROD;
+prodottonew.NomeProdotto = NOMEPROD;
+prodottonew.QntInMagazzino = QNTMAGAZZINO;
+
+
+
+
+Console.WriteLine("*********************");
+
+foreach (var prodotto in rub)
+{
+    Console.WriteLine($"nome: {prodotto.IdProdotto} - Nome: {prodotto.NomeProdotto} - Giacenza: {prodotto.QntInMagazzino}");
+}
+
+Console.WriteLine("*********************");
+
+
