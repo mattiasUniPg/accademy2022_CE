1e5653e65a8ccd205c85472d46d42fc99f5533ae
Matthias C <74117251+mattiasUniPg@users.noreply.github.com>
2022-04-29 14:13:09 +0200

    Add files via upload

---
+++ Accademy.ConsoleTest/Accademy.ConsoleTest/ExampleStatic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchLegacy
+{
+    public static class ExampleStatic
+    {
+
+        public const float pigreco = 3.14f;
+        public static int sum(int a, int b) => a + b;
+        public static int diff(int a, int b) => a - b;
+    }
+}
